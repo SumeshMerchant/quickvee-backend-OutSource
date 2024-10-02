5ca194173b4db753a628082408a255bef6fa9d0d
Hafiz Rizwan <rizwan.dev@pk.see.biz>
2024-10-02 17:27:48 +0500

    table of inventory

--- src/Components/Reporting/InventoryReport/FirstButtonSelections.js
+++ src/Components/Reporting/InventoryReport/FirstButtonSelections.js
@@ -76,7 +76,7 @@ const FirstButtonSelections = ({ selectedColumns, setSelectedColumns, applyColum
         }}
     >
 
-        <div style={{ paddingBlock: 8, paddingInline: 16, borderRadius: 8, fontSize: 14 }}>
+        <div style={{ paddingBlock: 8, paddingInline: 8, borderRadius: 8, fontSize: 14 }}>
             {/* <p className="mb-3" style={{ backgroundColor: '#F8F8F8', }}>              <Checkbox
                 sx={{
                     "& .MuiSvgIcon-root": {
@@ -106,7 +106,7 @@ const FirstButtonSelections = ({ selectedColumns, setSelectedColumns, applyColum
                 )}
                 Comment added
             </Grid>   */}
-            <p className="mb-3 d-flex align-center mb-20 padding-2"  style={{ backgroundColor: '#F8F8F8',gap: 12, paddingBlock: 9 }}>
+            <p className="mb-3 d-flex align-center mb-20 padding-2"  style={{ backgroundColor: '#F8F8F8',gap: 12, padding: 9 }}>
               <input
                 type="checkbox"
                 checked={selectAllColumns}
@@ -115,7 +115,7 @@ const FirstButtonSelections = ({ selectedColumns, setSelectedColumns, applyColum
               Select All
           </p>
           
-          <Grid container className="mb-3" spacing={3}>
+          <Grid container className="mb-3" sx={{ pl: 1 }} spacing={3}>
             <Grid item xs={6} sm={12} md={6} sx={{gap: 1.5, alignItems: 'center', display: 'flex'}}>
               
                 <input
@@ -199,7 +199,7 @@ const FirstButtonSelections = ({ selectedColumns, setSelectedColumns, applyColum
         Category
       </label> */}
             <div style={{ width: '100%', marginTop: 10, paddingBottom: 8 }}>
-                <button className="btn_blue" style={{ fontSize: 14 }} onClick={() => { applyColumns();  }}>
+            <button className="btn_blue" style={{ fontSize: 14 }} onClick={() => { applyColumns(); handleClose();  }}>
                     Apply
                 </button>
                 {/* <button onClick={() => setShowColumnPopup(false)}>Close</button> */}
