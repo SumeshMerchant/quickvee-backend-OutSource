a6bb659fc4354ae3ee86799cbeb240c6f22b84a8
absmd <smd.dev@pk.see.biz>
2024-10-02 18:28:41 +0800

    WIP: reorder report table changes

--- src/Components/Reporting/ReorderInventory/ReorderInventoryMain.js
+++ src/Components/Reporting/ReorderInventory/ReorderInventoryMain.js
@@ -1,26 +1,107 @@
-import React from "react";
+import React, { useState } from "react";
 import ReorderInventoryList from "./ReorderInventoryList";
 import { Grid } from "@mui/material";
+import SelectDropDown from "../../../reuseableComponents/SelectDropDown";
+import InventoryTable from "../InventoryReport/InventoryTable";
+import DashDateRangeComponent from "../../../reuseableComponents/DashDateRangeComponent";
 
 const ReorderInventoryMain = () => {
+  const [selectedDateRange, setSelectedDateRange] = useState(null);
+  const handleDateRangeChange = (dateRange) => {
+    setSelectedDateRange(dateRange);
+  };
+  const [selectedOrderSource, setSelectedOrderSource] = useState("Product");
+  const [selectedOrderType, setSelectedOrderType] =
+    useState("On-hand-inventory");
+
+  const handleOptionClick = (option, dropdown) => {
+    switch (dropdown) {
+      case "orderSource":
+        setSelectedOrderSource(option.title);
+
+        break;
+      case "orderType":
+        setSelectedOrderType(option.title);
+
+        break;
+      default:
+        break;
+    }
+  };
+  const showcat = 0;
+  const reportTypeList = [
+    "Product",
+    "SKU name",
+    "Brand",
+    "Outlet",
+    "Supplier",
+    "Product category",
+  ];
+  const measureTypeList = [
+    "On-hand-inventory",
+    "Low Inventory",
+    "Out of stock",
+  ];
+
+  const initialColumns = [
+    { id: "sku", name: "SKU" },
+    { id: "plus_after_sku", name: "+" },
+    { id: "name", name: "Name" },
+    { id: "closing_inventory", name: "Closing Inventory" },
+    { id: "items_sold", name: "Items Sold" },
+    { id: "days_cover", name: "Days Cover" },
+    { id: "avg_cost", name: "Avg Cost" },
+    { id: "plus_after_avg_cost", name: "+" },
+  ];
   return (
     <>
-      {/* <Grid
-        container
-        sx={{ padding: 2.5, mt: 3.6 }}
-        className="box_shadow_div "
-      >
+      <Grid container className="box_shadow_div">
         <Grid item xs={12}>
-          <Grid container>
+          <Grid container sx={{ px: 2.5, pt: 1 }}>
             <Grid item xs={12}>
-              <h1 style={{ marginBottom: 0 }} className="heading ">
-                Reorder Inventory Report
-              </h1>
+              <div className="heading">Filter By</div>
+            </Grid>
+          </Grid>
+          <Grid container spacing={2} sx={{ px: 2.5, pb: 2.5 }}>
+            <Grid item xs={12} sm={6} md={showcat != 0 ? 4 : 4}>
+              <label
+                className="q-details-page-label"
+                htmlFor="orderSourceFilter"
+              >
+                Report Type
+              </label>
+              <SelectDropDown
+                sx={{ pt: 0.5 }}
+                listItem={reportTypeList.map((item) => ({ title: item }))}
+                title={"title"}
+                dropdownFor={"orderSource"}
+                selectedOption={selectedOrderSource}
+                onClickHandler={handleOptionClick}
+              />
+            </Grid>
+            <Grid item xs={12} sm={6} md={showcat != 0 ? 4 : 4}>
+              <label
+                className="q-details-page-label"
+                htmlFor="orderSourceFilter"
+              >
+                Measure
+              </label>
+              <SelectDropDown
+                sx={{ pt: 0.5 }}
+                listItem={measureTypeList.map((item) => ({ title: item }))}
+                title={"title"}
+                dropdownFor={"orderType"}
+                selectedOption={selectedOrderType}
+                onClickHandler={handleOptionClick}
+              />
             </Grid>
           </Grid>
         </Grid>
-      </Grid> */}
-      <ReorderInventoryList sx={{ padding: 2.5, mt: 3.6 }} />
+      </Grid>
+      <Grid container sx={{}}>
+        <DashDateRangeComponent onDateRangeChange={handleDateRangeChange} />
+      </Grid>
+      <InventoryTable />
     </>
   );
 };
